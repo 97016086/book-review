79f105b77df37369713d51176caa3e97eb40d832
Renate <97016086@st.deltion.nl>
2024-02-09 13:30:02 +0100

    starting caching some data

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Book;
 use Illuminate\Http\Request;
 
+
 class BookController extends Controller
 {
     /**
@@ -29,7 +30,11 @@ class BookController extends Controller
 
         };
 
-        $books  = $books->get();
+        $cacheKey = 'books:'  . $filter . ':' . $title;
+        $books = cache()->remember($cacheKey, 3600, fn()  =>  $books->get());
+         
+          
+
 
         return  view('books.index', ['books'  =>  $books]);
     }
